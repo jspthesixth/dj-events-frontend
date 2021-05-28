d20211cb129fc459683783ed4100c1e9735adaea
Josip Graho <josip.graho1@gmail.com>
2021-05-28 14:42:38 +0200

    Create Layout, Header and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Layout.module.css'
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+import styles from '../styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -9,13 +11,15 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 Layout.defaultProps = {
   title: 'DJ Events | Find the hottest parties',
   description: 'Find the latest DJ and other musical events',
   keywords: 'music, dj, edm, events',
-}
+};
